4921a3d6b6fce3255fe59c871c537a3f5f7cfb91
barosanuemailtest <barosanu.email.test@gmail.com>
2021-05-03 13:01:25 +0300

    Basic Lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/lib/aws-lambda';
+import { join } from 'path';
 
 
 
@@ -7,7 +9,16 @@ export class SpaceStack extends Stack {
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
+
+
+
+        const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_14_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+            handler: 'hello.main'
+        })
     }
+
 
 
 }
---
+++ services/hello/hello.js
@@ -0,0 +1,9 @@
+
+
+
+exports.main = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: 'Hello from Lambda!'
+    }
+}

7a0020a75157e30ef932838a9c1898de19e04c84
barosanuemailtest <barosanu.email.test@gmail.com>
2021-05-05 18:31:42 +0300

    Using the AWS sdk

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/lib/aws-apigateway'
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/lib/aws-iam';
+
 
 
 export class SpaceStack extends Stack {
@@ -23,7 +25,10 @@ export class SpaceStack extends Stack {
             entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
             handler: 'handler'
         });
-
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*')
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
 
 
         // Hello Api lambda integration:
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,12 +1,16 @@
 import { v4 } from 'uuid'
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
 
 
 async function handler(event: any, context: any) {
+    const buckets = await s3Client.listBuckets().promise();
     console.log('Got an event:');
     console.log(event);
     return {
         statusCode: 200,
-        body: 'Hello from lambda!' + v4()
+        body: 'Here are your buckets: ' + JSON.stringify(buckets.Buckets)
     }
 }
 

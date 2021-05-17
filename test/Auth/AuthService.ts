860a1d486c2b17371d5c9da6b17453bc8496d8df
barosanuemailtest <barosanu.email.test@gmail.com>
2021-05-17 09:38:11 +0300

    Getting AWS temporary credentials

--- test/Auth/AuthService.ts
+++ test/Auth/AuthService.ts
@@ -2,6 +2,8 @@ import { Auth } from 'aws-amplify';
 import Amplify from 'aws-amplify';
 import { config } from './config';
 import { CognitoUser } from '@aws-amplify/auth'
+import * as AWS from 'aws-sdk';
+import { Credentials } from 'aws-sdk/lib/credentials';
 
 Amplify.configure({
     Auth: {
@@ -9,6 +11,7 @@ Amplify.configure({
         region: config.REGION,
         userPoolId: config.USER_POOL_ID,
         userPoolWebClientId: config.APP_CLIENT_ID,
+        identityPoolId: config.IDENTITY_POOL_ID,
         authenticationFlowType: 'USER_PASSWORD_AUTH'
     }
 })
@@ -22,5 +25,32 @@ export class AuthService {
     public async login(userName: string, password: string){
         const user = await Auth.signIn(userName, password) as CognitoUser;
         return user;
+    }
+
+
+    public async getAWSTemporaryCreds(user: CognitoUser){
+       const cognitoIdentityPool = `cognito-idp.${config.REGION}.amazonaws.com/${config.USER_POOL_ID}`; 
+       AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+           IdentityPoolId: config.IDENTITY_POOL_ID,
+           Logins: {
+               [cognitoIdentityPool]: user.getSignInUserSession()!.getIdToken().getJwtToken()
+           }
+       }, {
+           region: config.REGION
+       });
+       await this.refreshCredentials();
+    }
+
+
+    private async refreshCredentials(): Promise<void>{
+        return new Promise((resolve, reject)=>{
+            (AWS.config.credentials as Credentials).refresh(err =>{
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+        })
     }
 }
--- test/Auth/auth.test.ts
+++ test/Auth/auth.test.ts
@@ -1,8 +1,17 @@
 import { AuthService } from './AuthService';
 import { config } from './config';
+import * as AWS from 'aws-sdk';
 
 
 
-const authService = new AuthService();
+async function callStuff() {
+    const authService = new AuthService();
 
-const user = authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD)
+    const user = await authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+    await authService.getAWSTemporaryCreds(user);
+    const someCreds = AWS.config.credentials;
+    const a = 5;
+}
+
+callStuff();
+

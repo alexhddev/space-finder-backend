9b5fb01ad1d9c36db2b12fe82c28ca0bd8163438
barosanuemailtest <barosanu.email.test@gmail.com>
2021-05-21 15:38:08 +0300

    Testing AWS temporary credentials

--- test/Auth/auth.test.ts
+++ test/Auth/auth.test.ts
@@ -2,6 +2,17 @@ import { AuthService } from './AuthService';
 import { config } from './config';
 import * as AWS from 'aws-sdk';
 
+AWS.config.region = config.REGION;
+
+async function getBuckets(){
+    let buckets;
+    try {
+        buckets = await new AWS.S3().listBuckets().promise();
+    } catch (error) {
+        buckets = undefined
+    }
+    return buckets
+}
 
 
 async function callStuff() {
@@ -10,6 +21,7 @@ async function callStuff() {
     const user = await authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
     await authService.getAWSTemporaryCreds(user);
     const someCreds = AWS.config.credentials;
+    const buckets = await getBuckets();
     const a = 5;
 }
 
--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -4,7 +4,7 @@ export const config = {
     REGION: 'eu-west-1',
     USER_POOL_ID: 'eu-west-1_gRIUoOdLx',
     APP_CLIENT_ID: '335vma5jmludo42mn2895cu5bh',
-    IDENTITY_POOL_ID: 'eu-west-1:9c9a27eb-9a10-4e1c-98d2-892266b9a000',
+    IDENTITY_POOL_ID: 'eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599',
     TEST_USER_NAME: 'barosanu2',
     TEST_USER_PASSWORD: 'askjskfT7sdf&'
 }

e4fd69c0cac84e6b5569e5462007d9ee3cb80fcb
barosanuemailtest <9tonfr9lkb97A>
2021-08-11 14:07:01 +0300

    Debug config and Policies tests

--- test/infrastructure/Policies.test.ts
+++ test/infrastructure/Policies.test.ts
@@ -1,9 +1,37 @@
-
+import { Stack } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/lib/aws-s3';
+import { Policies } from '../../infrastructure/Policies';
+import '@aws-cdk/assert/jest';
+import { Effect } from 'aws-cdk-lib/lib/aws-iam';
 
 
 describe('Policies test suite', ()=>{
 
-    test('initial test', ()=>{
-        console.log('Testing works!');
-    })
+    const stack = new Stack();
+    const spacesPhotosBucket = {
+        bucketArn: 'spacesPhotosBucketArn'
+    };
+    const profilePhotosBucket = {
+        bucketArn: 'profilePhotosBucketArn'
+    };;
+    const policies: Policies = new Policies(spacesPhotosBucket as Bucket, profilePhotosBucket as Bucket);
+
+    test('UploadProfilePhoto policy values', ()=>{
+        const uploadProfilePhotoJSON = policies.uploadProfilePhoto.toJSON();
+        expect(uploadProfilePhotoJSON.Action).toEqual([
+            's3:PutObject',
+            's3:PutObjectAcl'
+        ])
+        expect(uploadProfilePhotoJSON.Effect).toBe('Allow');
+        expect(uploadProfilePhotoJSON.Resource).toBe('profilePhotosBucketArn/*');        
+    });
+    test('UploadSpacesPhoto policy values', ()=>{
+        const uploadSpacesPhotoJSON = policies.uploadSpacePhotos.toJSON();
+        expect(uploadSpacesPhotoJSON.Action).toEqual([
+            's3:PutObject',
+            's3:PutObjectAcl'
+        ])
+        expect(uploadSpacesPhotoJSON.Effect).toBe('Allow');
+        expect(uploadSpacesPhotoJSON.Resource).toBe('spacesPhotosBucketArn/*');        
+    });
 });
